814ea94c40dc5708b5edc82af35b34830083f1e9
Jin Lee <leejin21c@gmail.com>
2021-03-02 23:54:36 +0900

    change to scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
+import "./App.scss";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p className="logo-txt">J:PLAN</p>
+                <div className="logo-container">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p className="logo-txt">J:PLAN</p>
+                </div>
+                <p className="login">로그인</p>
             </header>
             <div className="Content">
                 <p className="Times">여기는 채워지는 시간표</p>
