04f2d98287f7fa421af52130ad4fa74d3113ca4e
Jin Lee <leejin21c@gmail.com>
2021-03-04 00:31:26 +0900

    find and fix input error

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+
 import styles from "./TodoList.module.scss";
 import ListItem from "./ListItem.js";
 
 const initTodo = [
-    { checked: true, content: "plan-app에서 todolist 컴포넌트 만들어주기" },
+    {
+        id: 1,
+        checked: true,
+        content: "plan-app에서 todolist 컴포넌트 만들어주기",
+    },
 ];
 
 const TodoList = () => {
-    const [task, setTask] = useState("");
+    const [task, setTask] = useState("안녕");
     const [todos, setTodos] = useState(initTodo);
+    const next_id = useRef(1);
+
     const onChange = (e) => {
-        setTask(e.target.task);
+        // console.log(e.target.value);
+        setTask(e.target.value);
+    };
+    const onClick = () => {
+        next_id.current++;
+        console.log(next_id);
+        setTodos([
+            ...todos,
+            { id: next_id.current, checked: false, content: task },
+        ]);
+        setTask("");
+        console.log(todos);
+    };
+    const handleKeyPress = (e) => {
+        // console.log(e.key);
+        if (e.key === "Enter") {
+            // console.log(e.key);
+            onClick();
+        }
     };
     return (
         <div className={styles.wrapper}>
@@ -19,10 +44,16 @@ const TodoList = () => {
                 name="task"
                 onChange={onChange}
                 autoFocus
+                onKeyPress={handleKeyPress}
             ></input>
+            <button onClick={onClick}>제출</button>
             <ul>
-                {todos.map(({ checked, content }) => (
-                    <ListItem checked={checked} content={content}></ListItem>
+                {todos.map(({ id, checked, content }) => (
+                    <ListItem
+                        checked={checked}
+                        content={content}
+                        key={id}
+                    ></ListItem>
                 ))}
             </ul>
         </div>
