a427a919e11d9c6f7b5cc04e170be56c4abcdcc4
Jin Lee <leejin21c@gmail.com>
2021-03-09 00:06:26 +0900

    make useTimer: refactoring and add icon

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,6 +3,11 @@ import React, { useState, useRef } from "react";
 import styles from "./TodoList.module.scss";
 import ListItem from "./ListItem.js";
 
+import { formatTime } from "../utils";
+import useTimer from "../hooks/useTimer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+
 const initTodo = [
     {
         id: 1,
@@ -11,18 +16,24 @@ const initTodo = [
     },
 ];
 
+const element = <FontAwesomeIcon icon={faClock} />;
+
 const TodoList = () => {
-    const [task, setTask] = useState("안녕");
+    const [task, setTask] = useState("");
     const [todos, setTodos] = useState(initTodo);
-    const [timer, setTimer] = useState(0);
-    // const [isActive, setIsActive] = useState(false);
-    // const [isPaused, setIsPaused] = useState(false);
-    const countRef = useRef(null);
+    const {
+        timer,
+        isActive,
+        isPaused,
+        handleStart,
+        handlePause,
+        handleResume,
+        handleReset,
+    } = useTimer();
 
     const next_id = useRef(1);
 
     const onChange = (e) => {
-        // console.log(e.target.value);
         setTask(e.target.value);
     };
     const onClick = () => {
@@ -44,37 +55,19 @@ const TodoList = () => {
             onClick();
         }
     };
-    // 스톱워치 관련 functions
-    const handleStart = () => {
-        // setIsActive(true);
-        // setIsPaused(true);
-        countRef.current = setInterval(() => {
-            setTimer((timer) => timer + 1);
-        }, 1000);
-    };
-    const handlePause = () => {
-        clearInterval(countRef.current);
-        // setIsPaused(false);
-    };
-    const handleResume = () => {
-        // setIsPaused(true);
-        countRef.current = setInterval(() => {
-            setTimer((timer) => timer + 1);
-        }, 1000);
-    };
-    const handleReset = () => {
-        clearInterval(countRef.current);
-        // setIsActive(false);
-        // setIsPaused(false);
-        setTimer(0);
-    };
+
     return (
         <div className={styles.wrapper}>
-            <p>총 공부량: {timer}</p>
+            <p>총 공부량: {element}</p>
+            <p>{formatTime(timer)}</p>
             <div>
-                <button onClick={handleStart}>Start</button>
-                <button onClick={handlePause}>Pause</button>
-                <button onClick={handleResume}>Resume</button>
+                {!isActive && !isPaused ? (
+                    <button onClick={handleStart}>Start</button>
+                ) : isPaused ? (
+                    <button onClick={handlePause}>Pause</button>
+                ) : (
+                    <button onClick={handleResume}>Resume</button>
+                )}
                 <button onClick={handleReset}>Reset</button>
             </div>
             <input
